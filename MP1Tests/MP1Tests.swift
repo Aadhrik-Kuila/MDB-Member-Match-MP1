f89014d251984d2afd6eef7c09d801822cde4dbc
Aadhrik Kuila <aadhrik.kuila@berkeley.edu>
2019-02-07 14:45:41 -0800

    Initial Commit

---
+++ MP1Tests/MP1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  MP1Tests.swift
+//  MP1Tests
+//
+//  Created by Aadhrik Kuila on 2/7/19.
+//  Copyright © 2019 Aadhrik Kuila. All rights reserved.
+//
+
+import XCTest
+@testable import MP1
+
+class MP1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
